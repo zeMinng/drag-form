d79e48f9c964bbd8f9b5b09337e47969697562a6
zeMing <2439340964@qq.com>
2025-07-15 16:07:48 +0800

    feat: antd国际化

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React, { Suspense } from 'react'
 import Router from './router'
-import { Spin } from 'antd'
+import { Spin, ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 import './App.css'
 // const LazyComponent = React.lazy(() => new Promise(() => {})); // 永远不resolve
 
 const App: React.FC = () => (
-  <div className="app">
-    {/* Suspense 用于懒加载时显示加载中状态 */}
-    <Suspense fallback={
-      <div className="center-loading">
-        <Spin size="large" />
-      </div>
-    }>
-      {/* <LazyComponent /> */}
-      <Router />
-    </Suspense>
-  </div>
+  <ConfigProvider locale={zhCN}>
+    <div className="app">
+      {/* Suspense 用于懒加载时显示加载中状态 */}
+      <Suspense fallback={
+        <div className="center-loading">
+          <Spin size="large" />
+        </div>
+      }>
+        {/* <LazyComponent /> */}
+        <Router />
+      </Suspense>
+    </div>
+  </ConfigProvider>
 )
 
 export default App
--- src/views/system/home.tsx
+++ src/views/system/home.tsx
@@ -1,5 +1,8 @@
+import { TimePicker } from "antd"
+import dayjs from 'dayjs'
+
 const Home: React.FC = () => {
-  return <h1>首页</h1>
+  return <TimePicker defaultOpenValue={dayjs('00:00:00', 'HH:mm:ss')} />
 }
 
 export default Home
