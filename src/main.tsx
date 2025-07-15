0bf86b8848e2e2320b65dcdb75743b25ddeb42bd
zeMing <2439340964@qq.com>
2025-07-15 21:56:04 +0800

    feat: 全局scss

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import '@ant-design/v5-patch-for-react-19'
 import 'antd/dist/reset.css'
 import App from './App.tsx'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
