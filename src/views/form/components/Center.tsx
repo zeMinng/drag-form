f3d1f02d0be249b8ddf59189b4c97094e11eb89d
zeMing <2439340964@qq.com>
2025-07-16 16:21:57 +0800

    feat: 样式优化

--- src/views/form/components/Center.tsx
+++ src/views/form/components/Center.tsx
@@ -1,8 +1,14 @@
+import '../style/Center.scss'
 
 const Center: React.FC = () => {
-  return <>
-    中间编辑区
-  </>
+  return (
+    <div className="center-container">
+      {/* 这里将来渲染表单组件 */}
+      <div className="center-placeholder">
+        请从左侧拖拽组件到这里
+      </div>
+    </div>
+  )
 }
 
 export default Center
