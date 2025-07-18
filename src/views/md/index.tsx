41a2039c6d4a7474cc1f9b48a3572c24d3d7b1d5
zeMing <2439340964@qq.com>
2025-07-18 15:46:45 +0800

    feat: 设计思路md

--- src/views/md/index.tsx
+++ src/views/md/index.tsx
@@ -2,7 +2,7 @@ import { MarkdownRenderer } from '@/components/MarkdownRenderer'
 
 const DesignIdeas: React.FC = () => {
   return (
-    <div>
+    <div style={{ width: '100%', height: '100%', overflowY: 'auto' }}>
       <MarkdownRenderer fileName="designIdeas.md" />
     </div>
   )
