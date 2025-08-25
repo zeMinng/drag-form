72e0fe120b29180513ee73efe5a272eb2f9c1f2e
zeMing <2439340964@qq.com>
2025-08-25 10:41:01 +0800

    feat: 404页面更新UI

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons'
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_4976950_ctqh4fyo9cs.js',
+  scriptUrl: '//at.alicdn.com/t/c/font_4976950_4ipixg3myuw.js',
 })
 
 export default IconFont
